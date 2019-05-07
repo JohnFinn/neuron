01544216631ed9ded9e2fa5c104cb3998f78bc38
Jouni Sunnari <Jouny@tuta.io>
2019-05-08 00:06:19 +0300

    changed target function

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,26 @@ use rand::*;
 mod net;
 use net::*;
 
+fn target1(x: bool, y: bool, z: bool) -> bool {
+    (x == y) || (!x && z)
+}
+
 fn target(x: f32) -> f32 {
     (1.0 + x.sin()).ln()
 }
 
 fn main() {
-    let mut a = net![2, 3, 1];
-    println!("~~~~~~~~~~~~~ network ~~~~~~~~~~~~~~~~~~ {0} =================================", a);
-    let train_data = vec![
-        DataPoint::new(dvec![0.0, 0.0], dvec![0.0]),
-        DataPoint::new(dvec![0.0, 1.0], dvec![1.0]),
-        DataPoint::new(dvec![1.0, 0.0], dvec![1.0]),
-        DataPoint::new(dvec![1.0, 1.0], dvec![0.0]),
-    ];
+    let mut a = net![3, 1];
+    let train_data: Vec<_> = (0..8)
+        .map(|a| ((a & 4) != 0, (a & 2) != 0, (a & 1) != 0))
+        .map(|(x, y, z)|((x, y, z), target1(x, y, z)))
+        .map(|((x, y, z), r)| ((x as i8 as f32, y as i8 as f32, z as i8 as f32), r as i8 as f32))
+        .map(|((x, y, z), r)| DataPoint {
+            input:  dvec![x, y, z],
+            output: dvec![r],
+        })
+        .collect();
+
     println!("untrained");
     for x in &train_data {
         let res = a.predict(x.input.clone());
@@ -30,6 +37,4 @@ fn main() {
         let res = a.predict(x.input.clone());
         println!("expected: {0} got {1}", x.output, res);
     }
-    let res = a.predict(dvec![1.0, 0.0]);
-    println!("~~~~~~~~~~~~~ network ~~~~~~~~~~~~~~~~~~ {0} =================================", a);
 }
